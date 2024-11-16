e48039096e4a6fd6cbb91214dcee20a663df6bc4
Ameera Adam <meeradam@bu.edu>
2024-11-16 17:02:55 -0500

    fixed bug in traversals, added list of friends to recomment

--- traverse_graphs.py
+++ traverse_graphs.py
@@ -32,11 +32,12 @@ class Graph:
         queue = deque([node])
         while queue:
             current_node = queue.popleft()
+            traversal_order.append(current_node)
             #print(current_node)
             for adjacent_node in self.adjacency_list[current_node]:
                 if adjacent_node not in visited:
                     visited.add(adjacent_node)
-                    traversal_order.append(current_node)
+
                     queue.append(adjacent_node)
         return traversal_order
 
@@ -56,18 +57,26 @@ class Graph:
 
         return traversal_order
     
-    def recommendFriends(self, traversal_order, node):
+    def getFriendList(self, traversal_order, node):
 
         direct_friends = set(self.adjacency_list.get(node, []))
         # print(direct_friends)
-        recommendations = {}
+        friend_list = {}
 
         for friend in traversal_order:
             if friend not in direct_friends and friend != node:
                 mutual_friends = len(direct_friends & set(self.adjacency_list.get(friend, [])))
-                recommendations[friend] = mutual_friends
+                friend_list[friend] = mutual_friends
             
-        return sorted(recommendations.items(), key=lambda x: -x[1])
+        return sorted(friend_list.items(), key=lambda x: -x[1])
+    
+    def recommendFriends(self, friend_list, n_mutual_friend):
+        recommended_friends = []
+        for i in range(len(friend_list)):
+            if int(friend_list[i][1]) >= n_mutual_friend:
+                recommended_friends.append(friend_list[i][0])
+        print(recommended_friends)
+        return recommended_friends
 
 
 
@@ -77,9 +86,13 @@ student_graph.loadGraph(student_graph_file)
 
 # BFS
 bfs_traversal_order = student_graph.bfs(student)
-print(student_graph.recommendFriends(bfs_traversal_order, student))
+friend_list = student_graph.getFriendList(bfs_traversal_order, student)
+#print(len(friend_list))
+recommended_friends = student_graph.recommendFriends(friend_list, 5)
+
 
 # DFS
-# dfs_traversal_order = student_graph.dfs(student)
-# print(student_graph.recommendFriends(dfs_traversal_order, student))
-
+dfs_traversal_order = student_graph.dfs(student)
+friend_list = student_graph.getFriendList(dfs_traversal_order, student)
+#print(len(friend_list))
+recommended_friends = student_graph.recommendFriends(friend_list, 5)
