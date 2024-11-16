acb78dd5746444716cfd2eab21d1b9f39c47ac60
Ameera Adam <meeradam@bu.edu>
2024-11-16 16:03:04 -0500

    added bfs traversal

--- traverse_graphs.py
+++ traverse_graphs.py
@@ -22,8 +22,26 @@ class Graph:
     def printGraph(self):
         for node, friends in self.adjacency_list.items():
             print(f"{node} : {friends}")
+    
+    def bfs(self, node):
+        visited = set()
+        visited.add(node)
+        traversal_order = []
+
+        queue = deque([node])
+        while queue:
+            current_node = queue.popleft()
+            #print(current_node)
+            for adjacent_node in self.adjacency_list[current_node]:
+                if adjacent_node not in visited:
+                    visited.add(adjacent_node)
+                    traversal_order.append(current_node)
+                    queue.append(adjacent_node)
+        return traversal_order
 
 
 student_graph = Graph()
 student_graph.loadGraph(student_graph_file)
-student_graph.printGraph()
+# student_graph.printGraph()
+
+student_graph.bfs('Kaylee Wise')
