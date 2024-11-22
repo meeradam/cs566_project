8cfb68635612a4e7ae51ca57839dccbb325b766c
Ameera Adam <meeradam@bu.edu>
2024-11-22 15:13:35 -0500

    moved to functions

--- generate_students.py
+++ generate_students.py
@@ -1,45 +1,48 @@
 import pandas as pd
 from faker import Faker
 import random
+from variables import *
 
 fake = Faker()
-boston_colleges = ['BU', 'BC', 'Harvard', 'MIT', 'NEU', 'UMass']
-max_friends = 100
-n_students = 500
 
-student_name = []
-student_college = []
-for i in range(0, n_students):
+def generate_student_list(n_students, max_friends):
 
-    # inital list of student names
-    name = fake.first_name() + " " +  fake.last_name()
-    student_name.append(name)
-    student_name = list(set(student_name))
+    student_name = []
+    student_college = []
 
-for i in range(len(student_name)):
-    student_college.append(random.choice(boston_colleges))
+    for i in range(0, n_students):
 
-d = {'student_name':student_name, 'college':student_college}
-df = pd.DataFrame(data = d)
+        # inital list of student names
+        name = fake.first_name() + " " +  fake.last_name()
+        student_name.append(name)
+        student_name = list(set(student_name))
+    for i in range(len(student_name)):
+        student_college.append(random.choice(boston_colleges))
 
-friend_list = []
-for i in df['student_name']:
-    # create a list of friends for student
-    student_friends = []
-    n_friends = random.randint(1, max_friends+1)
-    #print(n_friends)
+    d = {'student_name':student_name, 'college':student_college}
+    df = pd.DataFrame(data = d)
 
-    for j in range(n_friends):
-        friend = random.choice(student_name)
-        student_friends.append(friend)
-        # make sure all unique friends
-        student_friends = list(set(student_friends))
+    friend_list = []
+    for i in df['student_name']:
+        # create a list of friends for student
+        student_friends = []
+        n_friends = random.randint(1, max_friends+1)
+        #print(n_friends)
 
-        # drop name if itself
-        student_friends = [x for x in student_friends if x != i]
+        for j in range(n_friends):
+            friend = random.choice(student_name)
+            student_friends.append(friend)
+            # make sure all unique friends
+            student_friends = list(set(student_friends))
+
+            # drop name if itself
+            student_friends = [x for x in student_friends if x != i]
+        
+        friend_list.append(student_friends)
     
-    friend_list.append(student_friends)
+    friends = {'friends': friend_list}
+    df['friends'] = friend_list
+    # df.to_csv('boston_students.csv', index=None)
+    print(df.head())
 
-friends = {'friends': friend_list}
-df['friends'] = friend_list
-df.to_csv('boston_students.csv', index=None)
+#generate_student_list(50, 5)
---
+++ measure_time_complexity.py
@@ -0,0 +1,9 @@
+import time
+from generate_students import *
+from create_graph import *
+from traverse_graphs import *
+
+n_students = 20
+max_friends = 5
+
+generate_student_list(n_students, max_friends)
